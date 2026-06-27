14ff270ceba0124baf0bf27e8f576d6cb06034c9
Rajib Sedai <rajibsedhai@gmail.com>
2026-06-27 08:26:56 +0545

    logout out contollers using access and refresh token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -62,7 +62,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 userSchema.methods.generateAccessToken = function () {
     return token = jwt.sign(
         { 
-            id: this._id,
+            _id: this._id,
             email: this.email,
             username: this.username,
             fullName: this.fullName
@@ -73,7 +73,7 @@ userSchema.methods.generateAccessToken = function () {
 userSchema.methods.generateRefreshToken = function () {
    return token = jwt.sign(
         { 
-            id: this._id
+            _id: this._id
         }, 
         process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
     );
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
-import {registerUser} from "../controllers/user.controller.js";
+import {logoutUser, registerUser} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middlerware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(
@@ -9,5 +10,12 @@ router.route("/register").post(
         { name: "coverImage", maxCount: 1 }
     ]),
     registerUser);
+
+router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
+ //in verifyjwt we use next so that 
+// it can execute logoutuser after execution of lverifyjwt
 
 export default router;
