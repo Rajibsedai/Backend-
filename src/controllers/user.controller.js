006c75f78da94d4502e1351e86c814367abbf735
Rajib Sedai <rajibsedhai@gmail.com>
2026-07-01 08:50:51 +0545

    Mongo db aggergation pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -208,7 +208,7 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
 const changeCurrentPassword = asyncHandler(async (req,res) => {
 
   const {oldPassword,newPassword}=req.body;
-  const user= User.FindById(req.user?._id)
+  const user= await User.FindById(req.user?._id) //await halis
 
   const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
   
@@ -225,13 +225,13 @@ const changeCurrentPassword = asyncHandler(async (req,res) => {
   
 })
 
-const getCurrentUser = async (req,res) => {
+const getCurrentUser = asyncHandler(async (req,res) => {
   return res
   .status(200)
   .json(200,req,user,"current user fetched successfully")
 
   
-}
+})
 
 const updateAccountDetails = asyncHandler(async(req,res)=>{
   const {fullName, email}=req.body
@@ -241,7 +241,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
     throw new ApiError(400,"All field are required")
   }
 
-  const user = User.findByIdAndUpdate(req.user?._id,
+  const user = await User.findByIdAndUpdate(req.user?._id,
     {
       $set:{
         fullName:fullName,
@@ -311,11 +311,88 @@ const updateUserCoverImage = asyncHandler(async (req,res) => {
     },
     {new: true}
   ).select("-password")
-return res
-.status(200)
-.json(new ApiResponse(200,user,"Cover Image updated successfully"))
+  return res
+  .status(200)
+  .json(new ApiResponse(200,user,"Cover Image updated successfully"))
   
 })
 
-export {registerUser, loginUser, logoutUser, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage};
+
+const getUserChannelProfile = asyncHandler(async (req,res) => {
+  const {username} =req.params
+
+  if(!username){
+    throw new ApiError(400,"Username is required")
+  }
+  const channel = await User.aggregate([
+    {
+      $match:{
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup:{
+        from: "subscriptions",
+        localField:"_id",
+        foreignField:"channel",
+        as:"subscribers"
+
+      }
+    },
+    {
+      $lookup:{
+        from: "subscriptions",
+        localField:"_id",
+        foreignField:"subscriber",
+        as:"subscribedTo"
+    }
+  },
+    {
+      $addFields:{
+        subscriberCount: {
+          $size: "$subscribers"
+        },
+        channelsSubscribedToCount:{
+          $size:"$subscribedTo"
+        },
+        isSubscribed:{
+          $cond: {
+            if:{$in: [req.user?.id,"subscribers.subscriber"]},
+            then: true,
+            else: false
+          }
+        }
+      }
+
+    },
+    {
+      $project:{
+        fullName:1,
+        username:1,
+        subscriberCount:1,
+        channelsSubscribedToCount:1,
+        avatar:1,
+        coverImage:1,
+        email:1
+      }
+    }
+  ])
+//console.log(channel) to see what channel return
+
+if(!channel?.length){
+  throw new ApiError(404,"channeldoesnot exist")
+}
+
+return res
+.status(200)
+.json(
+  new ApiResponse(200, channel[0],"User channel fetched successfully")
+)
+})
+
+export {registerUser, loginUser, logoutUser,
+   changeCurrentPassword, getCurrentUser, 
+   updateAccountDetails, updateUserAvatar, updateUserCoverImage};
+
+  
 
